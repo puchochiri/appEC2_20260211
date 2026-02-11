38fd168ea78ad9073d7b9a9d8d5d232b243929b0
puchochiri2343 <doawishfor@gmail.com>
2026-02-11 13:42:55 +0900

    initial commit

---
+++ src/main/java/org/zerock/appec2_20260211/AppEc220260211Application.java
@@ -0,0 +1,13 @@
+package org.zerock.appec2_20260211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEc220260211Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppEc220260211Application.class, args);
+  }
+
+}
---
+++ src/test/java/org/zerock/appec2_20260211/AppEc220260211ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.appec2_20260211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEc220260211ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
